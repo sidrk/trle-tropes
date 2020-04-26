fd4baff708113dd2e54ae276f6b565a5d8b80575
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2020-04-26 10:45:44 -0700

    Add TR2 and TR3 level names

--- scripts/parse.js
+++ scripts/parse.js
@@ -19,8 +19,29 @@ function listTropes(json) {
   }
 }
 
+function listLevels(json) {
+  var levelsList = document.getElementById("levels-list");
+  for (levelName in json) {
+    var level = json[levelName];
+    var name = level.name;
+    console.log(level);
+
+    // Add the level to the list
+    var li = document.createElement("li");
+    var liName = document.createElement("div");
+    liName.innerHTML = name;
+    li.appendChild(liName);
+
+    levelsList.appendChild(li);
+  }
+}
+
 fetch("data/tropes.json")
   .then((response) => response.json())
   .then((json) => listTropes(json));
+
+fetch("data/levels.json")
+  .then((response) => response.json())
+  .then((json) => listLevels(json));
 
 console.log("Scripts loaded.");
